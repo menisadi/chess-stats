9ba886f46ca304c3af5709f24780fdfd5bf62c2c
menisadi <menisadi@gmail.com>
2023-07-09 23:47:17 +0300

    dash - removed reset button for now

--- sandbox/dash_animation.py
+++ sandbox/dash_animation.py
@@ -1,14 +1,14 @@
 import numpy as np
 import pandas as pd
 import dash
-from dash import dcc, html
+import dash_core_components as dcc
+import dash_html_components as html
 import plotly.express as px
 
 app = dash.Dash(__name__)
 
 sample_size = 100
 frames = 100
-starting_frame = 0
 
 # Create initial data
 data = pd.DataFrame({'value': np.random.normal(size=sample_size * frames),
@@ -22,7 +22,7 @@ fig = px.scatter(data_frame=data, x='value', y='hight', color=colors,
                  animation_frame='frame', width=800, height=300)
 fig.update_xaxes(range=[-4, 4])
 
-# Define the layout with the slider, reset button, and title
+# Define the layout with the slider and title
 app.layout = html.Div([
     dcc.Slider(
         id='sample-size-slider',
@@ -32,7 +32,6 @@ app.layout = html.Div([
         value=sample_size,
         marks={str(size): str(size) for size in range(10, 501, 50)}
     ),
-    html.Button('Reset', id='reset-button', n_clicks=0),
     html.H3(id='sample-size-title'),
     dcc.Graph(id='scatter-plot', figure=fig)
 ])
@@ -41,16 +40,10 @@ app.layout = html.Div([
 @app.callback(
     [dash.dependencies.Output('scatter-plot', 'figure'),
      dash.dependencies.Output('sample-size-title', 'children')],
-    [dash.dependencies.Input('sample-size-slider', 'value'),
-     dash.dependencies.Input('reset-button', 'n_clicks')]
+    [dash.dependencies.Input('sample-size-slider', 'value')]
 )
-def update_scatter_plot(sample_size, reset_clicks):
-    if reset_clicks > 0:
-        current_frame = starting_frame
-    else:
-        current_frame = (reset_clicks % frames) + starting_frame
-
-    # Generate new data based on the updated sample_size and current_frame
+def update_scatter_plot(sample_size):
+    # Generate new data based on the updated sample_size
     data = pd.DataFrame({'value': np.random.normal(size=sample_size * frames),
                          'hight': np.random.uniform(size=sample_size * frames),
                          'frame': np.arange(frames).repeat(sample_size)})
@@ -62,8 +55,8 @@ def update_scatter_plot(sample_size, reset_clicks):
                      animation_frame='frame', width=800, height=300)
     fig.update_xaxes(range=[-4, 4])
 
-    # Update the title with the current sample size and frame
-    title = f"Sample Size: {sample_size}, Current Frame: {current_frame}"
+    # Update the title with the current sample size
+    title = f"Sample Size: {sample_size}"
 
     return fig, title
 
